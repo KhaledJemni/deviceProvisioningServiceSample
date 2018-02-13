96f50889a02145aa50d5168e9787f6477fcfb219
Khaled Jemni <jekhaled@outlook.com>
2018-02-13 15:15:12 +0100

    Add project files.

---
+++ deviceProvisioningServiceSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace deviceProvisioningServiceSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
